97bccfee9f8eac598edaa1538bd5b371f398ec99
Ioannis Kokkinis <ioanniskokkinis@mac-mini.home>
2015-11-14 20:19:49 +0000

    Initial Commit

---
+++ GreekTV/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GreekTV
+//
+//  Created by Ioannis Kokkinis on 14/11/2015.
+//  Copyright © 2015 upg.gr. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
